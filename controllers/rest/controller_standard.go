677382266ed1d1c08540eb5cea1ee41ac3b68960
shaniah868 <shaniahworld@hotmail.com>
2022-01-18 09:55:17 -0400

    feature: WEOS-1132 Add function to handler and invoke event dispatcher with correct information -Added code to get etag from database

--- controllers/rest/controller_standard.go
+++ controllers/rest/controller_standard.go
@@ -2,8 +2,10 @@ package rest
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/segmentio/ksuid"
@@ -125,11 +127,13 @@ func (c *StandardControllers) Update(app model.Service, spec *openapi3.Swagger, 
 	return func(ctxt echo.Context) error {
 		//look up the schema for the content type so that we could identify the rules
 		newContext := ctxt.Request().Context()
+		cType := &context2.ContentType{}
 		if contentType != "" && contentTypeSchema.Value != nil {
-			newContext = context.WithValue(newContext, context2.CONTENT_TYPE, &context2.ContentType{
+			cType = &context2.ContentType{
 				Name:   contentType,
 				Schema: contentTypeSchema.Value,
-			})
+			}
+			newContext = context.WithValue(newContext, context2.CONTENT_TYPE, cType)
 		}
 		var weosID string
 		var newPayload map[string]interface{}
@@ -164,31 +168,73 @@ func (c *StandardControllers) Update(app model.Service, spec *openapi3.Swagger, 
 		var Etag string
 		var identifiers []string
 		var result *model.ContentEntity
+		var result1 map[string]interface{}
 		if etagInterface == nil {
+			//find entity based on identifiers specified
 			pks, _ := json.Marshal(contentTypeSchema.Value.Extensions["x-identifier"])
 			json.Unmarshal(pks, &identifiers)
 			if len(identifiers) == 0 {
 				identifiers = append(identifiers, "id")
 			}
-			//TODO use the getbyprimarykey projection function, and add etag
-		} else {
+			primaryKeys := map[string]interface{}{}
+			for _, p := range identifiers {
+				primaryKeys[p] = newContext.Value(p)
+			}
 
 			for _, projection := range app.Projections() {
 				if projection != nil {
-					result, err := projection.GetContentEntity(newContext, weosID)
+					result1, err = projection.GetByKey(newContext, cType, primaryKeys)
 					if err != nil {
 						return err
 					}
-					Etag = NewEtag(result)
+
 				}
 			}
+			weos_id, ok := result1["weos_id"].(string)
+			sequenceString := fmt.Sprint(result1["sequence_no"])
+			sequenceNo, _ := strconv.Atoi(sequenceString)
+			Etag = NewEtag(&model.ContentEntity{
+				AggregateRoot: model.AggregateRoot{
+					SequenceNo:  int64(sequenceNo),
+					BasicEntity: model.BasicEntity{ID: weos_id},
+				},
+			})
+			if (len(result1) == 0) || !ok || weos_id == "" {
+				return NewControllerError("No entity found", err, http.StatusNotFound)
+			} else if err != nil {
+				return NewControllerError(err.Error(), err, http.StatusBadRequest)
+			}
+			result1["weos_id"] = ""
+			result1["SequenceNo"] = 0
+
+			ctxt.Response().Header().Set("Etag", Etag)
+
+			return ctxt.JSON(http.StatusOK, result)
+		} else {
+			//find contentEntity based on weosid
+			for _, projection := range app.Projections() {
+				if projection != nil {
+					result, err = projection.GetContentEntity(newContext, weosID)
+					if err != nil {
+						return err
+					}
+
+				}
+			}
+			if result == nil || result.ID == "" {
+				return NewControllerError("No entity found", err, http.StatusNotFound)
+			} else if err != nil {
+				return NewControllerError(err.Error(), err, http.StatusBadRequest)
+			}
+			Etag = NewEtag(result)
+			result.ID = ""
+			result.SequenceNo = 0
+
+			ctxt.Response().Header().Set("Etag", Etag)
+
+			return ctxt.JSON(http.StatusOK, result)
 		}
-		result.ID = ""
-		result.SequenceNo = 0
 
-		ctxt.Response().Header().Set("Etag", Etag)
-
-		return ctxt.JSON(http.StatusOK, result)
 	}
 }
 
