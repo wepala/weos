209d260c53171f8cfe607cf09490f92a20a5e2fd
shaniah868 <shaniahworld@hotmail.com>
2022-02-28 14:14:04 -0400

    feature: WEOS-1343 As a developer I should be able to setup an OpenID Connect security scheme -Change the order of the loop

--- controllers/rest/global_initializers.go
+++ controllers/rest/global_initializers.go
@@ -11,17 +11,19 @@ import (
 func Security(ctxt context.Context, api *RESTAPI, swagger *openapi3.Swagger) (context.Context, error) {
 	middlewares := GetOperationMiddlewares(ctxt)
 	found := false
-	for key, scheme := range swagger.Components.SecuritySchemes {
-		//checks if the security scheme has type openIdConnect
-		if scheme.Value.Type == "openIdConnect" {
-			for _, security := range swagger.Security {
-				if security[key] != nil {
+
+	for _, security := range swagger.Security {
+		for key, _ := range security {
+			if swagger.Components.SecuritySchemes != nil && swagger.Components.SecuritySchemes[key] != nil {
+				//checks if the security scheme has type openIdConnect
+				if swagger.Components.SecuritySchemes[key].Value.Type == "openIdConnect" {
 					found = true
 					break
 				}
-			}
 
+			}
 		}
+
 	}
 	if found {
 		if middleware, _ := api.GetMiddleware("OpenIDMiddleware"); middleware != nil {
