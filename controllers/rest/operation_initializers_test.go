a2b34ac86e4009bed4d4400f8e68181a78d47927
shaniah868 <65876481+shaniah868@users.noreply.github.com>
2022-04-13 12:07:44 -0400

    Changes by randy feature: WEOS-1496 As a developer I should be able to setup an OpenID Connect security scheme on a path -Added unit test for adding security on path

--- controllers/rest/operation_initializers_test.go
+++ controllers/rest/operation_initializers_test.go
@@ -398,3 +398,53 @@ func TestGettersForOperationFunctions(t *testing.T) {
 		}
 	})
 }
+
+func TestSecurityPathInitializer(t *testing.T) {
+	api, err := rest.New("./fixtures/blog-security-path.yaml")
+	if err != nil {
+		t.Fatalf("unexpected error loading api '%s'", err)
+	}
+	schemas := rest.CreateSchema(context.TODO(), api.EchoInstance(), api.Swagger)
+	baseCtxt := context.WithValue(context.TODO(), weoscontext.SCHEMA_BUILDERS, schemas)
+	api.Schemas = schemas
+
+	middlewareCalled := false
+	api.RegisterMiddleware("OpenIDMiddleware", func(api *rest.RESTAPI, projection projections.Projection, commandDispatcher model.CommandDispatcher, eventSource model.EventRepository, entityFactory model.EntityFactory, path *openapi3.PathItem, operation *openapi3.Operation) echo.MiddlewareFunc {
+		return func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
+			return func(c echo.Context) error {
+				middlewareCalled = true
+				return nil
+			}
+		}
+	})
+
+	t.Run("security set on a path", func(t *testing.T) {
+
+		ctxt, err := rest.SecurityPathInitializer(baseCtxt, api, "/blogs", http.MethodPost, api.Swagger, api.Swagger.Paths["/blogs"], api.Swagger.Paths["/blogs"].Post)
+		if err != nil {
+			t.Fatalf("unexpected error loading api '%s'", err)
+		}
+
+		middleware := rest.GetOperationMiddlewares(ctxt)
+		if middleware == nil {
+			t.Fatalf("expected middleware to be in the context")
+		}
+
+		middlewares := rest.GetOperationMiddlewares(ctxt)
+		if len(middlewares) != 1 {
+			t.Fatalf("expected the middlewares in context to be %d, got %d", 1, len(middlewares))
+		}
+		for _, middleware := range middlewares {
+			err = middleware(api, nil, nil, nil, nil, nil, nil)(func(c echo.Context) error {
+				return nil
+			})(echo.New().AcquireContext())
+			if err != nil {
+				t.Errorf("unexpected error running middleware '%s'", err)
+			}
+		}
+		if !middlewareCalled {
+			t.Errorf("expected middleware to be in context and called")
+		}
+
+	})
+}
