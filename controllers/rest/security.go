40ddaed7577f3b969cdbdea0c7dc25e58de420ae
Ishika_Gopie <ishikagopie123@gmail.com>
2023-05-18 11:18:25 -0400

    fix: added a log error message for when success is false when checking if the role has access for the endpoint but no error occurred

--- controllers/rest/security.go
+++ controllers/rest/security.go
@@ -144,10 +144,9 @@ func (s *SecurityConfiguration) Middleware(api Container, commandDispatcher mode
 						if success {
 							return next(ctxt)
 						}
-						if err != nil {
+						if err != nil || !success {
 							ctxt.Logger().Errorf("the role '%s' does not have access to '%s' action '%s': original error", role, tpath, ctxt.Request().Method, err)
 						}
-						ctxt.Logger().Errorf("the role '%s' does not have access to '%s' action '%s': original error", role, tpath, ctxt.Request().Method, err)
 						return ctxt.NoContent(http.StatusForbidden)
 					}
 					return next(ctxt)
